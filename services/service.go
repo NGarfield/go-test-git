1b40140f2683e68a6694276bbef579d4c0d1ec62
DESKTOP-P1432GE\nack <nackzas458@gmail.com>
2022-02-25 10:53:55 +0700

    add get all test

--- services/service.go
+++ services/service.go
@@ -1,1 +1,27 @@
 package services
+
+type DBer interface{
+	GetAllTest() ([]Test, error)
+}
+
+type Servicer interface{
+	GetAllService() ([]Test, error)
+}
+
+type Service struct{
+	db DBer
+}
+
+func NewService(db DBer) *Service{
+	return &Service{
+		db: db,
+	}
+}
+
+func (s *Service) GetAllService() ([]Test, error){
+	result, err := s.db.GetAllTest()
+	if err != nil {
+		return nil,err
+	}
+	return result,nil
+}
