90074344c52cc2f9872a778257cd9d00a06459e8
flookbozo <f_l_o_o_k@hotmail.com>
2022-02-25 11:17:59 +0700

    add insert service

--- repository/repository.go
+++ repository/repository.go
@@ -10,17 +10,30 @@ type DB struct {
 	db *gorm.DB
 }
 
-func New(db *gorm.DB) *DB{
+func New(db *gorm.DB) *DB {
 	return &DB{
 		db: db,
 	}
 }
 
-func (db *DB) GetAllTest() ([]services.Test, error){
- 	var test []services.Test
+func (db *DB) GetAllTest() ([]services.Test, error) {
+	var test []services.Test
 	result := db.db.Find(&test)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	return test,nil
+	return test, nil
+}
+
+func (r *DB) InsertTestData(name, lastname string, age uint) error {
+	test := services.Test{
+		Name:     name,
+		Lastname: lastname,
+		Age:      age,
+	}
+	tx := r.db.Create(&test)
+	if tx.Error != nil {
+		return tx.Error
+	}
+	return nil
 }
--- services/service.go
+++ services/service.go
@@ -1,27 +1,33 @@
 package services
 
-type DBer interface{
+type DBer interface {
 	GetAllTest() ([]Test, error)
+	InsertTestData(name, lastname string, age uint) error
 }
 
-type Servicer interface{
+type Servicer interface {
 	GetAllService() ([]Test, error)
+	InsertTestDataService(req *Test) error
 }
 
-type Service struct{
+type Service struct {
 	db DBer
 }
 
-func NewService(db DBer) *Service{
+func NewService(db DBer) *Service {
 	return &Service{
 		db: db,
 	}
 }
 
-func (s *Service) GetAllService() ([]Test, error){
+func (s *Service) GetAllService() ([]Test, error) {
 	result, err := s.db.GetAllTest()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return result,nil
+	return result, nil
+}
+
+func (s *Service) InsertTestDataService(req *Test) error {
+	return s.db.InsertTestData(req.Name, req.Lastname, req.Age)
 }
